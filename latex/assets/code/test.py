0e7aee8b05579fed820692e15f8e1ca15055964f
ecwu <bitlabwzh@gmail.com>
2019-06-24 17:14:28 +0800

    Fix confidential message

---
+++ latex/assets/code/test.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
